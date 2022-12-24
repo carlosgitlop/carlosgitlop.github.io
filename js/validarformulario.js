1ddf863ba58681dad19e588178b860bad2d4e3c4
carlosgitlop <carlos.lopezgomez@mengibar.eu>
2022-12-24 10:19:22 +0100

    Comentarios explicativos en validarformulario

--- js/validarformulario.js
+++ js/validarformulario.js
@@ -6,16 +6,16 @@ function validarFormulario(evento) {
   evento.preventDefault();
   
   let nombre = document.getElementById ('nombre').value;
-  let nombresinespacios = nombre.replace(/\s/g,""); // almaceno en nombressinespacios para no contabilizar los espacios en blanco
-  if(!isNaN(nombresinespacios) || nombresinespacios.length < 4) {
+  let nombresinespacios = nombre.replace(/\s/g,""); // almaceno en 'nombressinespacios' para no contabilizar los espacios en blanco
+  if(!isNaN(nombresinespacios) || nombresinespacios.length < 4) { // hago que no puedan introducir números y mínimo 4 letras
     alert('El nombre debe tener como mínimo 4 letras');
     return;
   }
 
   let apellidos = document.getElementById ('apellidos').value;
-  let apellidossinespacios = apellidos.replace(/\s/g,"");  // almaceno en apellidossinespacio para no contabilizar los espacios en blanco
-  if(!isNaN(apellidossinespacios) || apellidossinespacios.length < 8) {  // falta hacer 4 caracteres mínimo cada apellido
-    alert('Los apellidos debe tener como mínimo 4 letras cada uno');
+  let apellidossinespacios = apellidos.replace(/\s/g,"");  // almaceno en 'apellidossinespacio' para no contabilizar los espacios en blanco
+  if(!isNaN(apellidossinespacios) || apellidossinespacios.length < 8) {  // hago que no puedan introducir números y mínimo 4 letras
+    alert('Los apellidos debe tener como mínimo 4 letras cada uno'); // me falta hacer 4 letras mínimo CADA APELLIDO (ahora funciona con 5 letras el primer apellido, un espacio y 3 letras el segundo, por ejemplo)
     return;
   }
 
@@ -29,17 +29,19 @@ function validarFormulario(evento) {
   let emailvalido = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;  // este es más genérico /^(.+\@.+\..+)$/
 
 	if(!emailvalido.test(email) ){
-		alert('El email introducido no es válido');
+		alert('El email introducido no es válido. Por favor introduce un formato de email válido');
 		return;
 	}
 
   let tel = document.getElementById ('tel').value;
-  let telvalido = /^\(\+\d{2,3}\)\d{9}$/; // adaptar para que solamente coja el 34, ahora coge cualquiera de dos cifras
+  let telvalido = /^\(\+\d{2,3}\)\d{9}$/; // me falta adaptar para que solamente coja el 34, ahora coge cualquiera de dos cifras
 	
   if(!telvalido.test(tel) ){
-		alert('El teléfono introducido no es válido');
+		alert('El teléfono introducido no es válido. Utiliza el formato (+34) seguido de 9 dígitos (todo escrito sin poner ningún espacio)');
 		return;
 	}  
-  
+
 this.submit();
+alert('El formulario se ha enviado correctamente');
+
 }
